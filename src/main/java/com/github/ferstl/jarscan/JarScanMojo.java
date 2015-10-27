9c9b024f241f117a5b68112c657f965bea091892
Stefan Ferstl <st.ferstl@gmail.com>
2015-10-27 13:23:35 -0700

    Add include/exclude/scope filtering

--- src/main/java/com/github/ferstl/jarscan/JarScanMojo.java
+++ src/main/java/com/github/ferstl/jarscan/JarScanMojo.java
@@ -3,18 +3,26 @@ package com.github.ferstl.jarscan;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import org.adoptopenjdk.jitwatch.jarscan.JarScan;
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.resolver.filter.AndArtifactFilter;
+import org.apache.maven.artifact.resolver.filter.ArtifactFilter;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.artifact.filter.ScopeArtifactFilter;
+import org.apache.maven.shared.artifact.filter.StrictPatternExcludesArtifactFilter;
+import org.apache.maven.shared.artifact.filter.StrictPatternIncludesArtifactFilter;
 
 @Mojo(
     name = "scan",
@@ -30,20 +38,67 @@ public class JarScanMojo extends AbstractMojo {
   @Component
   private MavenProject project;
 
+  /**
+   * The scope of the artifacts that should be included.
+   *
+   * @since 1.0.0
+   */
+  @Parameter(property = "scope")
+  private String scope;
+
+  /**
+   * Comma-separated list of artifacts to be included in the form of {@code groupId:artifactId:type:classifier}.
+   *
+   * @since 1.0.0
+   */
+  @Parameter(property = "includes", defaultValue = "")
+  private List<String> includes;
+
+  /**
+   * Comma-separated list of artifacts to be excluded in the form of {@code groupId:artifactId:type:classifier}.
+   *
+   * @since 1.0.0
+   */
+  @Parameter(property = "excludes", defaultValue = "")
+  private List<String> excludes;
+
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
     @SuppressWarnings("unchecked")
     Set<Artifact> dependencies = this.project.getDependencyArtifacts();
+
+    ArtifactFilter filter = createArtifactFilter();
+
     for (Artifact dependency : dependencies) {
-      try {
-        System.out.println("Scanning " + dependency);
-        JarScan.iterateJar(dependency.getFile(), 325, new PrintWriter(new OutputStreamWriter(System.out)));
-        System.out.println("done: " + dependency);
-        System.out.println();
-      } catch (IOException e) {
-        throw new MojoExecutionException(e.getMessage());
+      if (filter.include(dependency)) {
+        try {
+          System.out.println("Scanning " + dependency);
+          JarScan.iterateJar(dependency.getFile(), 325, new PrintWriter(new OutputStreamWriter(System.out)));
+          System.out.println("done: " + dependency);
+          System.out.println();
+        } catch (IOException e) {
+          throw new MojoExecutionException(e.getMessage());
+        }
       }
     }
+  }
+
+  private ArtifactFilter createArtifactFilter() {
+    List<ArtifactFilter> filters = new ArrayList<>(3);
+
+    if (this.scope != null) {
+      filters.add(new ScopeArtifactFilter(this.scope));
+    }
+
+    if (!this.includes.isEmpty()) {
+      filters.add(new StrictPatternIncludesArtifactFilter(this.includes));
+    }
+
+    if (!this.excludes.isEmpty()) {
+      filters.add(new StrictPatternExcludesArtifactFilter(this.excludes));
+    }
+
+    return new AndArtifactFilter(filters);
   }
 
 }
