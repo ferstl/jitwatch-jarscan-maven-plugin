d03491aca25a1cdad9abb8d252971f8b01f2bea4
Stefan Ferstl <st.ferstl@gmail.com>
2016-05-06 09:55:55 +0200

    [#3] Add support for methodLengthHisto
    methodLength List methods of the given bytecode size.
      --length=n Size of methods to find.

---
+++ src/main/java/com/github/ferstl/jarscan/MethodLengthMojo.java
@@ -0,0 +1,38 @@
+package com.github.ferstl.jarscan;
+
+import org.adoptopenjdk.jitwatch.jarscan.IJarScanOperation;
+import org.adoptopenjdk.jitwatch.jarscan.methodlength.MethodLengthOperation;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+/**
+ * List methods of the given bytecode size.
+ *
+ * @since 1.1
+ */
+@Mojo(
+    name = "methodlength",
+    aggregator = false,
+    defaultPhase = LifecyclePhase.VERIFY,
+    requiresDependencyCollection = ResolutionScope.TEST,
+    requiresDependencyResolution = ResolutionScope.TEST,
+    requiresDirectInvocation = false,
+    threadSafe = true)
+public class MethodLengthMojo extends AbstractJarScanMojo {
+
+  /**
+   * Size of methods to find.
+   *
+   * @since 1.1
+   */
+  @Parameter(property = "length", defaultValue = "1")
+  private int length;
+
+  @Override
+  protected IJarScanOperation createOperation() {
+    return new MethodLengthOperation(this.length);
+  }
+
+}
