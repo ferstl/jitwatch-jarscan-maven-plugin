0ee1809b3be992853fbabac2db42da4c846ab166
Stefan Ferstl <st.ferstl@gmail.com>
2016-05-06 09:55:11 +0200

    [#3] Add support for instructionCount
    instructionCount Count occurences of each bytecode instruction.
         [--limit=n] Limit to top n results.

---
+++ src/main/java/com/github/ferstl/jarscan/InstructionCountMojo.java
@@ -0,0 +1,38 @@
+package com.github.ferstl.jarscan;
+
+import org.adoptopenjdk.jitwatch.jarscan.IJarScanOperation;
+import org.adoptopenjdk.jitwatch.jarscan.instructioncount.InstructionCountOperation;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+/**
+ * Count occurrences of each bytecode instruction.
+ *
+ * @since 1.1
+ */
+@Mojo(
+    name = "instructionCount",
+    aggregator = false,
+    defaultPhase = LifecyclePhase.VERIFY,
+    requiresDependencyCollection = ResolutionScope.TEST,
+    requiresDependencyResolution = ResolutionScope.TEST,
+    requiresDirectInvocation = false,
+    threadSafe = true)
+public class InstructionCountMojo extends AbstractJarScanMojo {
+
+  /**
+   * Limit to top n results.
+   *
+   * @since 1.1
+   */
+  @Parameter(property = "limit", defaultValue = "0")
+  private int limit;
+
+  @Override
+  protected IJarScanOperation createOperation() {
+    return new InstructionCountOperation(this.limit);
+  }
+
+}
