8c48a1ce2435a4411b9a4872bfcd9fc4c74ab333
Stefan Ferstl <st.ferstl@gmail.com>
2016-05-06 09:54:40 +0200

    [#3] Add Support for nextInstructionFreq
    nextInstructionFreq List the most popular next instruction for each bytecode instruction.
            [--limit=n] Limit to top n results per instruction.

---
+++ src/main/java/com/github/ferstl/jarscan/NextInstructionFreqMojo.java
@@ -0,0 +1,38 @@
+package com.github.ferstl.jarscan;
+
+import org.adoptopenjdk.jitwatch.jarscan.IJarScanOperation;
+import org.adoptopenjdk.jitwatch.jarscan.nextinstruction.NextInstructionOperation;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+/**
+ * List the most popular next instruction for each bytecode instruction.
+ *
+ * @since 1.1
+ */
+@Mojo(
+    name = "nextInstructionFreq",
+    aggregator = false,
+    defaultPhase = LifecyclePhase.VERIFY,
+    requiresDependencyCollection = ResolutionScope.TEST,
+    requiresDependencyResolution = ResolutionScope.TEST,
+    requiresDirectInvocation = false,
+    threadSafe = true)
+public class NextInstructionFreqMojo extends AbstractJarScanMojo {
+
+  /**
+   * Limit to top n results per instruction.
+   *
+   * @since 1.1
+   */
+  @Parameter(property = "limit", defaultValue = "0")
+  private int limit;
+
+  @Override
+  protected IJarScanOperation createOperation() {
+    return new NextInstructionOperation(this.limit);
+  }
+
+}
